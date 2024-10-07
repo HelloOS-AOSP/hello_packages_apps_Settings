18cf5db02a355b66790013687851f241d9e5d8c9
LuK1337 <priv.luk@gmail.com>
2024-10-06 21:10:14 -0300

    Settings: Add HelloOS entries into device info
    Change-Id: I4acf834442712954f8e9db15370bffafd4547174

---
+++ src/com/android/settings/deviceinfo/firmwareversion/HelloBuildMaintainerPreferenceController.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2019-2024 The Evolution X Project
+ * Copyright (C) 2024 HelloOS-AOSP
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.android.settings.deviceinfo.firmwareversion;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
+import androidx.preference.Preference;
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+public class HelloBuildMaintainerPreferenceController extends BasePreferenceController {
+    private static final String TAG = "HelloBuildMaintainerCtrl";
+    private String mDeviceMaintainer;
+    public HelloBuildMaintainerPreferenceController(Context context, String key) {
+        super(context, key);
+        mDeviceMaintainer = mContext.getResources().getString(R.string.build_maintainer_summary);
+    }
+    @Override
+    public int getAvailabilityStatus() {
+        if (mDeviceMaintainer.equalsIgnoreCase("UNKNOWN")) {
+            return UNSUPPORTED_ON_DEVICE;
+        }
+        return AVAILABLE;
+    }
+    @Override
+    public CharSequence getSummary() {
+        return mDeviceMaintainer;
+    }
+}
